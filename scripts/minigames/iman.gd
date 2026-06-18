6c603b05b4f44fec49f703fa2dee0d4558d8b62a
Nuno Monteiro <1160944@isep.ipp.pt>
2026-06-18 20:35:11 +0100

    fix iman minigame

--- scripts/minigames/iman.gd
+++ scripts/minigames/iman.gd
@@ -1,57 +1,52 @@
-extends Control
+extends Node2D
 
-@export var min_clicks: int = 7
-@export var max_clicks: int = 10
-@export var reset_time: float = 0.5
-@export var button_start_scale: float = 1.0
-@export var button_end_scale: float = 1.5
+@onready var grid_container: GridContainer = $Control/GridContainer
+@onready var line: Line2D = $Line2D
+@onready var status_label: Label = $Control/Label
+@onready var experitation_timer = $Control/Timer
 
-@onready var button = $Button
-@onready var progress_bar = $ProgressBar
-@onready var label_counter = $Label
-@onready var reset_timer = $Timer
-@onready var experitation_timer = $Timer
+@export var line_color: Color = Color.RED
+@export var line_width: float = 10.0
 
-var target_clicks: int = 0
-var current_clicks: int = 0
+var buttons: Array = []
+var clicked_order: Array = []
 var is_complete: bool = false
+var button_positions: Array[Vector2] = []
 
 func _ready():
 	Minigames.is_in_minigame(true)
-	target_clicks = randi_range(min_clicks, max_clicks)
-	current_clicks = 0
-	update_ui()
-	reset_timer.wait_time = reset_time
-	reset_timer.one_shot = true
+	setup()
 
-func _on_button_pressed():
+func setup():
+	line.default_color = line_color
+	line.width = line_width
+	line.clear_points()
+	
+	buttons = grid_container.get_children()
+	await get_tree().process_frame
+	button_positions.clear()
+	for btn in buttons:
+		button_positions.append(btn.global_position + btn.size / 2)
+		btn.mouse_entered.connect(_on_button_hover.bind(buttons.find(btn)))
+		btn.pressed.connect(_on_button_hover.bind(btn)) 
+
+func _on_button_hover(index: int):
 	if is_complete:
 		return
-	current_clicks += 1
-	reset_timer.stop()
-	update_ui()
-	if current_clicks >= target_clicks:
+	if index in clicked_order:
+		return
+	clicked_order.append(index)
+	var pos = line.to_local(button_positions[index])
+	line.add_point(pos)
+	if clicked_order.size() == buttons.size():
 		complete_minigame()
-	else:
-		reset_timer.start()
-
-func update_ui():
-	var progress = float(current_clicks) / float(target_clicks)
-	progress_bar.value = progress
-	
-	var color = Color.WHITE.lerp(Color(0.8, 0.6, 0.0), progress)
-	button.modulate = color
-	button.scale = Vector2(button_start_scale + (button_end_scale - button_start_scale) * progress, button_start_scale + (button_end_scale - button_start_scale) * progress)
-	
-	label_counter.text = str(current_clicks) + " / " + str(target_clicks)
 
 func complete_minigame():
 	is_complete = true
-	reset_timer.stop()
 	Minigames.complete_minigame()
 	Global.rpc("add_iman_powerup")
 
-func _on_timer_2_timeout() -> void:
+func _on_timer_timeout() -> void:
 	if is_complete:
 		return
 	SoundEffects.lose_minigame_sound()
